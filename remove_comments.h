44f09d25231a7340aeeb0dfda528687b9ac7d7fc
pierredyl <dylanpierre004@gmail.com>
2025-02-16 15:15:46 -0800

    Refactored remove_comments to be its own class

---
+++ remove_comments.h
@@ -0,0 +1,14 @@
+//
+// Created by Dylan Pierre on 2/12/25.
+//
+
+#ifndef REMOVE_COMMENTS_H
+#define REMOVE_COMMENTS_H
+
+#include <fstream>
+using namespace std;
+
+void remove_comments(ifstream& inputFile, ofstream& outputFile);
+
+
+#endif //REMOVE_COMMENTS_H
